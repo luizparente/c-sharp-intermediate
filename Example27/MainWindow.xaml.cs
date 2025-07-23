dcd9ce0d0b0895c4163aaedb1429a6b7c8c09c70
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2025-07-23 12:16:05 -0400

    updating Example 27.

--- Example27/MainWindow.xaml.cs
+++ Example27/MainWindow.xaml.cs
@@ -11,12 +11,10 @@ namespace Example27
 
         private void OnSignIn(object sender, EventArgs e)
         {
-            // Named controls can be accessed in the code-behind like any other class field.
-            // Since txtName is a TextBox, we can retrieve its current value from property 'Text'.
-
             string username = this.txtUsername.Text;
-            string password = this.pwdPassword.Password; // <-- NO! This is terribly insecure. 
+            string password = this.pwdPassword.Password; // <-- NOOOOOOO!!!!
 
+            // Authentication logic.
             if (username == "john" && password == "123")
             {
                 MessageBox.Show("Login successful!", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
